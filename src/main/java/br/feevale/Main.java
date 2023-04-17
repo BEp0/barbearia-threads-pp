c7952257b7cc28ca66e1a68644534f7c5dcbdbcb
mees.ivan <mees.ivan@hotmail.com>
2023-04-16 23:57:15 -0300

    feat: adicionando teste barbearia com cadeiras e clientes

--- src/main/java/br/feevale/Main.java
+++ src/main/java/br/feevale/Main.java
@@ -1,22 +1,57 @@
 package br.feevale;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static class Pessoa {
-        public static String nome;
+    public static void main(String[] args) throws InterruptedException {
 
-        public Pessoa(String nome) {
-            this.nome = nome;
-        }
+        Barbearia barbearia = new Barbearia();
+
+        Barbeiro barbeiroA = new Barbeiro("santosA", EstadoBarbeiro.LIVRE);
+        Barbeiro barbeiroB = new Barbeiro("santosB", EstadoBarbeiro.LIVRE);
+        Barbeiro barbeiroC = new Barbeiro("santosC", EstadoBarbeiro.LIVRE);
+
+        Cadeira cadeiraA = new Cadeira();
+        cadeiraA.barbeiro = barbeiroA;
+
+        Cadeira cadeiraB = new Cadeira();
+        cadeiraB.barbeiro = barbeiroB;
+
+        Cadeira cadeiraC = new Cadeira();
+        cadeiraC.barbeiro = barbeiroC;
+
+        List<Cadeira> cadeiras = new ArrayList<>(3);
+
+        cadeiras.add(cadeiraA);
+        cadeiras.add(cadeiraB);
+        cadeiras.add(cadeiraC);
+
+        barbearia.cadeiras = cadeiras;
+        barbearia.iniciarExpediente();
+
+        Cliente bernardo = new Cliente();
+        bernardo.nome = "bernardo";
+        bernardo.tempoDeCorte = 5000L;
+
+        Cliente ivan = new Cliente();
+        ivan.nome = "ivan";
+        ivan.tempoDeCorte = 5000L;
+
+        Cliente lucas = new Cliente();
+        lucas.nome = "lucas";
+        lucas.tempoDeCorte = 10000L;
+
+
+        barbearia.receberCliente(List.of(bernardo,ivan,lucas));
+
+
+//        Thread.sleep(20000L);
+//        barbearia.receberCliente(bernardo);
+//        barbearia.receberCliente(ivan);
+//        barbearia.receberCliente(lucas);
+
     }
 
-
-    public static void main(String[] args) {
-
-        Pessoa bernardo = new Pessoa("bernardo");
-        Pessoa ivan = new Pessoa("ivan");
-
-        System.out.println(bernardo.nome);
-        System.out.println(Pessoa.nome);
-    }
 }
