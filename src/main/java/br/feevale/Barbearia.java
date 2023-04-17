48c7aaac47e6b6377dc630c6565d4253b91cda42
mees.ivan <mees.ivan@hotmail.com>
2023-04-16 23:58:05 -0300

    refactor: abstaindo logica salaEspera

--- src/main/java/br/feevale/Barbearia.java
+++ src/main/java/br/feevale/Barbearia.java
@@ -7,61 +7,46 @@ public class Barbearia {
 
     private static final int CAPACIDADE_MAXIMA_DE_BARBEIROS = 3;
 
-    public List<Cadeira> clientesCortando = new ArrayList<>(CAPACIDADE_MAXIMA_DE_BARBEIROS);
+    public List<Cadeira> cadeiras = new ArrayList<>(CAPACIDADE_MAXIMA_DE_BARBEIROS);
     public SalaDeEspera salaDeEspera = new SalaDeEspera();
 
 
     public void iniciarExpediente() {
-        for (Cadeira c : clientesCortando) {
+        for (Cadeira c : cadeiras) {
             String msg = String.format(">>>>> %s iniciando o expediente", c.barbeiro.nome);
             System.out.println(msg);
-            c.barbeiro.start();
+            c.start();
         }
     }
 
-//    public void receberCliente(Cliente cliente) {
-////        if (/*haBarbeirosLivres()*/true) {
-////            // barbeiro pega o cliente
-////
-////            System.out.println("Tem barbeiro livre!!!");
-////
-////            cliente.estado = Estado.CORTANDO;
-//////            clientesCortando.add(cliente);
-//////            Barbeiro b = getBarbeiroLivre();
-////
-////            String msg = String.format(">>>> %s está sentado para cortar o cabelo com %s!!!", cliente.nome);
-////            System.out.println(msg);
-////
-////        } else if (sofaEstaLivre()) {
-////            // cliente fica sentado
-////            System.out.println("Tem vaga no sofa!!!");
-////            clientesSentados.add(cliente);
-////
-////        } else if (haEspacoParaFicarDePe()) {
-////            // cliente fica de pe
-////            System.out.println("Tem vaga de pé!!!");
-////        } else {
-////            throw new BarbeariaLotadaException("Barbearia está lotada");
-////        }
-//    }
+    public void receberCliente(List<Cliente> clientes) throws InterruptedException {
+        for(Cliente cliente: clientes){
+            if (haBarbeirosLivres()) {
+                // barbeiro pega o cliente
+                System.out.println("Tem barbeiro livre!!!");
+                cliente.estado = Estado.CORTANDO;
+                Cadeira cadeira = getCadeiraLivre();
+                cadeira.cliente = cliente;
+                cadeira.liberarCorte();
 
-//    private Barbeiro getBarbeiroLivre() {
-//        return barbeiros.stream()
-//                .filter(barbeiro -> barbeiro.estado.equals(LIVRE))
-//                .findFirst()
-//                .orElseThrow();
-//    }
+                String msg = String.format(">>>> %s está sentado para cortar o cabelo com %s!!!", cliente.nome, cadeira.barbeiro.nome);
+                System.out.println(msg);
+            }else{
+                salaDeEspera.receberCliente(cliente);
+            }
+        }
+    }
 
-//    private synchronized boolean haEspacoParaFicarDePe() {
-//        return clientesDePe.isEmpty() || clientesDePe.size() < CAPACIDADE_MAXIMA_DE_CLIENTES_EM_PE;
-//    }
-//
-//    private synchronized boolean sofaEstaLivre() {
-//        return clientesSentados.isEmpty() || clientesSentados.size() < CAPACIDADE_MAXIMA_SOFA;
-//    }
+    private Cadeira getCadeiraLivre() {
+        return cadeiras.stream()
+                .filter(cadeira -> cadeira.barbeiro.estado.equals(EstadoBarbeiro.LIVRE))
+                .findFirst()
+                .orElseThrow();
+    }
 
-//    private synchronized boolean haBarbeirosLivres() {
-//        return barbeiros.stream()
-//                .anyMatch(barbeiro -> barbeiro.estado.equals(LIVRE));
-//    }
+
+    private synchronized boolean haBarbeirosLivres() {
+        return cadeiras.stream()
+                .anyMatch(cadeira -> cadeira.barbeiro.estado.equals(EstadoBarbeiro.LIVRE));
+    }
 }
--- src/main/java/br/feevale/SalaDeEspera.java
+++ src/main/java/br/feevale/SalaDeEspera.java
@@ -9,4 +9,28 @@ public class SalaDeEspera {
     public List<Cliente> clientesSentados = new ArrayList<>(CAPACIDADE_MAXIMA_SOFA);
     public List<Cliente> clientesDePe = new ArrayList<>(CAPACIDADE_MAXIMA_DE_CLIENTES_EM_PE);
 
+
+    public void receberCliente(Cliente cliente) {
+         if (sofaEstaLivre()) {
+            // cliente fica sentado
+            System.out.println("Tem vaga no sofa!!!");
+            clientesSentados.add(cliente);
+
+        } else if (haEspacoParaFicarDePe()) {
+            // cliente fica de pe
+            System.out.println("Tem vaga de pé!!!");
+            clientesDePe.add(cliente);
+        } else {
+            throw new BarbeariaLotadaException("Barbearia está lotada");
+        }
+    }
+
+
+    private synchronized boolean haEspacoParaFicarDePe() {
+        return clientesDePe.isEmpty() || clientesDePe.size() < CAPACIDADE_MAXIMA_DE_CLIENTES_EM_PE;
+    }
+
+    private synchronized boolean sofaEstaLivre() {
+        return clientesSentados.isEmpty() || clientesSentados.size() < CAPACIDADE_MAXIMA_SOFA;
+    }
 }
