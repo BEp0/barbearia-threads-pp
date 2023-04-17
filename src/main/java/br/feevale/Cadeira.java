12ad0de936e961019d0d0fab02491c61259aaac5
mees.ivan <mees.ivan@hotmail.com>
2023-04-16 23:56:08 -0300

    feat: implementando Thread

--- src/main/java/br/feevale/Cadeira.java
+++ src/main/java/br/feevale/Cadeira.java
@@ -1,12 +1,34 @@
 package br.feevale;
 
-public class Cadeira {
+import static br.feevale.EstadoBarbeiro.LIVRE;
+
+public class Cadeira extends Thread{
     public Barbeiro barbeiro;
     public Cliente cliente;
-    EstadoBarbeiro estado;
+//    EstadoBarbeiro estado;
 
 
-    public void iniciarCorte() {
+    @Override
+    public synchronized void run() {
+        while(true){
+            try {
+                this.wait();
+                barbeiro.estado = EstadoBarbeiro.CORTANDO;
+                Thread.sleep(cliente.tempoDeCorte);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            barbeiro.estado = EstadoBarbeiro.LIVRE;
+        }
+    }
 
+    public synchronized void liberarCorte(){
+        this.notify();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        barbeiro.estado = LIVRE;
     }
 }
